867789dbf84a241ac08d390de11fdbeaedb8fd1f
Knut Zoch <knut.zoch@gmail.com>
2017-11-28 13:13:10 +0100

    Add getters/setters for Argument class

--- argparse/argument.h
+++ argparse/argument.h
@@ -8,6 +8,12 @@ class Argument {
   Argument(const std::string& name,
            const std::string& help);
 
+  inline auto getName() const { return name_; }
+  inline auto getHelpMessage() const { return help_; }
+  inline auto getValue() const { return value_; }
+
+  inline void setValue(const std::string& value) { value_ = value; }
+
  protected:
   const std::string name_;
   const std::string help_;
