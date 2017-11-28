36cabe6fd369c05145d78cc981104182ee3f87b3
Knut Zoch <knut.zoch@gmail.com>
2017-11-28 12:49:05 +0100

    Implement storage of arguments in the argparser

--- argparse/argparse.h
+++ argparse/argparse.h
@@ -1,7 +1,11 @@
 #ifndef ARGPARSE_H_
 #define ARGPARSE_H_
 
+#include "argument.h"
+
+#include <memory>
 #include <string>
+#include <vector>
 
 class ArgumentParser {
  public:
@@ -10,6 +14,9 @@ class ArgumentParser {
   void addArgument(const std::string& name, const std::string& help, bool optional);
 
   void parse(int argc, char** argv);
+
+ protected:
+  std::vector<std::unique_ptr<Argument> > arguments_;
 };
 
 #endif  // ARGPARSE_H_
--- src/argparse.cc
+++ src/argparse.cc
@@ -8,6 +8,11 @@ void ArgumentParser::addArgument(const std::string& name,
                                  const std::string& help,
                                  bool optional) {
   // content here
+  if (optional) {
+    arguments_.emplace_back(std::make_unique<OptionalArgument>(name, help));
+  } else {
+    arguments_.emplace_back(std::make_unique<Argument>(name, help));
+ }
 }
 
 void ArgumentParser::parse(int argc, char** argv) {
