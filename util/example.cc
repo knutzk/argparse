79723dedfe64caf097e3acaa846055a51d5cc124
Knut Zoch <knut.zoch@gmail.com>
2017-12-31 18:29:02 +0900

    Catch exceptions and print what's wrong

--- util/example.cc
+++ util/example.cc
@@ -8,8 +8,15 @@ int main(int argc, char** argv) {
   parser.addArgument("--detailed", "Make everything detailed");
   parser.addArgument("--ignore", "Ignore everything");
   parser.getArgument("--ignore")->setDefaultValue("defval");
-  parser.parse(argc, argv);
 
-  std::cout << "Hello World" << std::endl;
+  try {
+    parser.parse(argc, argv);
+  } catch (const std::invalid_argument& s) {
+    std::cerr << "Parsing arguments failed with:\n  ";
+    std::cerr << s.what() << std::endl;
+    return -1;
+  }
+
+  std::cout << "Exiting program gracefully." << std::endl;
   return 0;
 }
